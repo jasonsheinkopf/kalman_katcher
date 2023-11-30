5fee24ab7996dc3fd226c70c87faf9eff0cd69c3
jasonsheinkopf <jasonsheinkopf@gmail.com>
2023-11-30 17:52:14 +0900

    began refactor

--- target_tracker.py
+++ target_tracker.py
@@ -3,23 +3,23 @@ from picamera import PiCamera
 import time
 import cv2
 import numpy as np
-from rpi_ws281x import Adafruit_NeoPixel, Color
 from time import sleep
-import RPi.GPIO as GPIO
 import pigpio
 import pickle
 import math
 import threading
-# import sys
 from kalman_filter import Catcher
 
 class Target:
+    """
+    Class to track individual white ping-pong ball
+    """
     def __init__(self, id, x, y, r):
         self.id = id
         self.x = x
         self.y = y
+        # detected radius
         self.r = r
-        self.threshold = 10
         # Kalman filter belief covariance matrix
         self.P = None
         # Kalman filter belief matrix
@@ -34,52 +34,49 @@ class Target:
     def __repr__(self):
         return f'{self.id}: ({self.x}, {self.y})'
     
-    def is_tracking(self, circle_x, circle_y):
-        distance = math.sqrt((int(circle_x) - int(self.x))**2 + (int(circle_y) - int(self.y))**2)
-
-        return True if distance < self.threshold else False
-    
     def find_distance(self, circle):
         return math.sqrt((int(circle[0]) - int(self.x))**2 + (int(circle[1]) - int(self.y))**2)
 
-def servo_dot(servo_box_l, servo_box_r, color):
+def servo_dot(goal_left_point, goal_right_point, color):
+    """
+    Used for creating a lookup table from catcher location to servo PWM frequency.
+    Servo box are left endpoint of goal. Updates global dot_x.
+    """
     global dot_x
-    x1, y1 = servo_box_l
-    x2, y2 = servo_box_r
-    # sample pixels along line
+    x1, y1 = goal_left_point
+    x2, y2 = goal_right_point
+
+    # list to hold sample points along goal line
     line_values = []
 
-    # create sample points along servo path
+    # create equally spaced points along goal line
     for t in np.linspace(0, 1, 100):
         x = int(x1 + t * (x2 - x1))
         y = int(y1 + t * (y2 - y1))
         line_values.append(image[y, x])
 
-    # points brighter than threshold are where the servo is
-    threshold = 200
+    # points brighter than threshold are part of white servo arm
+    BRIGHT_THRESHOLD = 200
 
     # find indices of bright pixels
-    bright_indices = np.where(np.array(line_values) > threshold)
+    bright_indices = np.where(np.array(line_values) > BRIGHT_THRESHOLD)
 
     # if there are no bright regions
     if bright_indices[0].size == 0:
-        print("Servo not in sight")
+        print("Catcher arm not intersecting goal line.")
     else:
         # find centerpoint
         center_index = np.array(bright_indices[0]).mean().astype(int)
         x_center = int(x1 + np.linspace(0, 1, 100)[center_index] * (x2 - x1))
         y_center = int(y1 + np.linspace(0, 1, 100)[center_index] * (y2 - y1))
-
-        # print(f'x: {x_center}, y: {y_center}')
-
         # x distance from dot to center
         dot_x_distance_to_center = x_center - (width / 2)
         # normalize dist to center to max at 1
         norm_dist_to_center = dot_x_distance_to_center / (width / 2)
-        # coefficient to adjust dot location by
-        dot_adj_coeff = 10
+        # coefficient to adjust dot location to counteract camera perspective
+        DOT_ADJ_COEFF = 10
         # adjustment
-        dot_adj = int(norm_dist_to_center * dot_adj_coeff)
+        dot_adj = int(norm_dist_to_center * DOT_ADJ_COEFF)
         # adjust dot x
         dot_x_adjusted = x_center + dot_adj
         # dot_x = x_center
@@ -87,13 +84,59 @@ def servo_dot(servo_box_l, servo_box_r, color):
         # Draw a circle at the center point
         cv2.circle(image, (dot_x, y_center), 10, color, -1)
 
-# LED config
-LED_COUNT = 12
-LED_PIN = 21
-LED_FREQ_HZ = 800000
-LED_DMA = 10
-LED_BRIGHTNESS = 255
-LED_INVERT = False
+def save_params(dict):
+    # save circle detection parameters to pickle for key press adjustment
+    with open('memory.pkl', 'wb') as file:
+        pickle.dump(dict, file)
+
+def calibrate_corners(gray):
+    """
+    Calibrates edges and goal line using template matching. Returns corner and goal endpoints.
+    """
+    # corner template images
+    bl_template = cv2.imread('templates/BL.jpg', 0).astype('uint8')
+    tl_template = cv2.imread('templates/TL.jpg', 0).astype('uint8')
+    br_template = cv2.imread('templates/BR.jpg', 0).astype('uint8')
+    tr_template = cv2.imread('templates/TR.jpg', 0).astype('uint8')
+
+    # corner template sizes
+    bl_h, bl_w = bl_template.shape
+    tl_h, tl_w = tl_template.shape
+    br_h, br_w = br_template.shape
+    tr_h, tr_w = tr_template.shape
+
+    # distance for bounding box from marker
+    edge = 5
+
+    # bottom left
+    result = cv2.matchTemplate(gray, bl_template, cv2.TM_CCOEFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+    location = max_loc
+    bottom_left = (location[0] - edge, location[1] + bl_h)
+    # top left
+    result = cv2.matchTemplate(gray, tl_template, cv2.TM_CCOEFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+    location = max_loc  
+    top_left = (location[0] - edge, location[1])
+    # top right
+    result = cv2.matchTemplate(gray, tr_template, cv2.TM_CCOEFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+    location = max_loc
+    top_right = (location[0] + tr_w + edge, location[1])
+    # bottom right
+    result = cv2.matchTemplate(gray, br_template, cv2.TM_CCOEFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+    location = max_loc  
+    bottom_right = (location[0] + br_w + edge, location[1] + br_h)
+
+    # servo edge
+    goal_left_point = (bottom_left[0], bottom_left[1] + GOAL_Y_OFFSET)
+    goal_right_point = (bottom_right[0], bottom_right[1] + GOAL_Y_OFFSET)
+
+    return top_left, bottom_right, top_right, bottom_left, goal_left_point, goal_right_point
+
+# set exit event to end thread
+exit_event = threading.Event()
 
 # camera frame rate
 framerate = 40
@@ -104,112 +147,27 @@ catcher = Catcher(dt=1/framerate)
 # list of colors to draw circles
 colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (255, 0, 255)]
 
-def save_values(dict):
-    # Save the dictionary to a Pickle file
-    with open('memory.pkl', 'wb') as file:
-        pickle.dump(dict, file)
+# servo PWM frequencies
+MIN_PWM = 750
+MAX_PWM = 2100
 
-def calibrate(gray):
-    # All the 6 methods for comparison in a list
-    methods = [cv2.TM_CCOEFF, cv2.TM_CCOEFF_NORMED, cv2.TM_CCORR,
-    cv2.TM_CCORR_NORMED, cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]
+# initialize goal position at middle or range
+goal_pos = (MAX_PWM - MIN_PWM) / 2
 
-    chosen_method = methods[1]
-
-    # distance for bounding box from marker
-    edge = 5
-
-    img2 = gray.copy()
-
-    # bottom left
-    result = cv2.matchTemplate(img2, bl_template, chosen_method)
-    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-    location = max_loc
-    bottom_left = (location[0] - edge, location[1] + bl_h)
-    cv2.rectangle(image, location, bottom_left, 255, 5)
-    # cv2.circle(image, bottom_left, 5, (0, 255, 0), 2)
-
-    # top left
-    result = cv2.matchTemplate(img2, tl_template, chosen_method)
-    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-    location = max_loc  
-    top_left = (location[0] - edge, location[1])
-    cv2.rectangle(image, location, top_left, 255, 5)
-    # cv2.circle(image, top_left, 5, (0, 255, 0), 2)
-
-    # top right
-    result = cv2.matchTemplate(img2, tr_template, chosen_method)
-    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-    location = max_loc
-    top_right = (location[0] + tr_w + edge, location[1])
-    cv2.rectangle(image, location, top_right, 255, 5)
-    # cv2.circle(image, top_left, 5, (0, 255, 0), 2)
-
-    # bottom right
-    result = cv2.matchTemplate(img2, br_template, chosen_method)
-    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-    location = max_loc  
-    bottom_right = (location[0] + br_w + edge, location[1] + br_h)
-    cv2.rectangle(image, location, bottom_right, 255, 5)
-    # cv2.circle(image, bottom_right, 5, (0, 255, 0), 2)
-
-    # Define a region of interest (ROI) based on the bounding box
-    roi = gray[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]]
-
-    # servo edge
-    servo_box_l = (bottom_left[0], bottom_left[1] + servo_line_offset)
-    servo_box_r = (bottom_right[0], bottom_right[1] + servo_line_offset)
-
-    # world height
-    world_height = math.sqrt((int(top_left[0]) - int(bottom_left[0]))**2 + (int(top_left[1]) - int(bottom_left[1]))**2)
-    world_width = math.sqrt((int(top_left[0]) - int(top_right[0]))**2 + (int(top_left[1]) - int(top_right[1]))**2)
-    world_aspect_ratio = world_width / world_height
-
-    # source points for transformation matrix
-    source_points = [top_left, servo_box_r, top_right, servo_box_l]
-
-    # new height and width
-    grid_h = 100
-    grid_w = grid_h * world_aspect_ratio
-
-    # grid points for mapping
-    grid_points = [(0, grid_h), (grid_w, 0), (grid_w, grid_h), (0, 0)]
-
-    # Create a perspective transformation matrix
-    trans_matrix = cv2.getPerspectiveTransform(np.float32(source_points), np.float32(grid_points))
-
-    return top_left, bottom_right, top_right, bottom_left, roi, servo_box_l, servo_box_r, trans_matrix
-
-# # create neopixel ring
-# ring = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
-# ring.begin()
-
-# white = Color(255, 255, 255)
-# for i in range(LED_COUNT):
-#     ring.setPixelColor(i, white)
-#     ring.show()
-
-# set exit event to end thread
-exit_event = threading.Event()
-
-count = 0
-
-# initial servo position
-min_pos = 750
-max_pos = 2100
-goal_pos = (max_pos - min_pos) / 2
-servo_line_offset = 30
+# how many pixels below bottom goal line is
+GOAL_Y_OFFSET = 30
 
 # global variable for current target
 current_target = None
 
 # create lookup dict to convert dot_x to goal_pos
 try:
-    # Try to load the Pickle file
+    # load lookup talbe
     with open('lookup.pkl', 'rb') as file:
         lookup_dict = pickle.load(file)
 except FileNotFoundError:
     # If the file doesn't exist, create an empty dictionary
+    print("Servo lookup table lookup.pkl not found. Press 't' to create.")
     lookup_dict = {}
 
 # servo dot location
@@ -235,7 +193,7 @@ def transform_coordinates(coord, transformation_matrix):
     new_coord = new_coordinates[0][0]
     return (new_coord[0], new_coord[1])
 
-def choose_target(new_targets, trans_matrix):
+def choose_target(new_targets):
     global current_target
     # print("choose target")
     # if there are targets
@@ -311,11 +269,11 @@ def servo_thread_fn():
     while not exit_event.is_set():
         if lookup_is_saved == False:
             # start servo at min position
-            goal_pos = min_pos
+            goal_pos = MIN_PWM
             # create empty lookup dict
             lookup_dict = {}
             # continue to sweep until it hits mas pos
-            while goal_pos < max_pos:
+            while goal_pos < MAX_PWM:
                 # send servo to active position
                 go_to_position(goal_pos)
                 # delay
@@ -355,7 +313,7 @@ def servo_thread_fn():
             goal_pos = lookup_dict[closest_key]
         else:
             # if no target is seen, return to middle point
-            goal_pos = (max_pos + min_pos) / 2
+            goal_pos = (MAX_PWM + MIN_PWM) / 2
 
         # send servo to active position
         go_to_position(goal_pos)
@@ -382,17 +340,7 @@ camera.shutter_speed = 4500
 rawCapture = PiRGBArray(camera, size=(width, height))
 time.sleep(1)
 
-# corner template images
-bl_template = cv2.imread('/home/jasonsheinkopf/Desktop/object_track/BL.jpg', 0).astype('uint8')
-tl_template = cv2.imread('/home/jasonsheinkopf/Desktop/object_track/TL.jpg', 0).astype('uint8')
-br_template = cv2.imread('/home/jasonsheinkopf/Desktop/object_track/BR.jpg', 0).astype('uint8')
-tr_template = cv2.imread('/home/jasonsheinkopf/Desktop/object_track/TR.jpg', 0).astype('uint8')
 
-# corner template sizes
-bl_h, bl_w = bl_template.shape
-tl_h, tl_w = tl_template.shape
-br_h, br_w = br_template.shape
-tr_h, tr_w = tr_template.shape
 
 # Set a threshold for matching
 gamma = .73
@@ -449,26 +397,26 @@ try:
         # set key press length
         key = cv2.waitKey(1) & 0xFF
 
-        # calibrate corners on first frame or key press
+        # calibrate_corners corners on first frame or key press
         if frame_num == 0 or key == ord('c'):
-            top_left, bottom_right, top_right, bottom_left, roi, servo_box_l, servo_box_r, trans_matrix = calibrate(gray)   
+            top_left, bottom_right, top_right, bottom_left, goal_left_point, goal_right_point = calibrate_corners(gray)   
             # boundaries for bounce
             boundaries = {
                 'left': (top_left[0] + bottom_left[0]) / 2,
                 'right': (top_right[0] + bottom_right[0]) / 2,
                 'top': (top_left[1] + top_right[1]) / 2,
-                'goal': (servo_box_l[1] + servo_box_r[1]) / 2,
-                'goal_l_point': servo_box_l,
-                'goal_r_point': servo_box_r
+                'goal': (goal_left_point[1] + goal_right_point[1]) / 2,
+                'goal_l_point': goal_left_point,
+                'goal_r_point': goal_right_point
             }
-            goal_y = int((servo_box_l[1] + servo_box_r[1]) / 2)
+            goal_y = int((goal_left_point[1] + goal_right_point[1]) / 2)
             print('Calibrating')
         
         if not lookup_is_saved:
             # set dot color to match current target color
             dot_color = (255, 0, 0)
             # show servo location dot
-            servo_dot(servo_box_l, servo_box_r, dot_color)
+            servo_dot(goal_left_point, goal_right_point, dot_color)
 
         # draw bounding box
         cv2.line(image, top_left, top_right, (0, 255, 255), 1)
@@ -479,7 +427,7 @@ try:
         # set goal color to match current target color
         goal_color = colors[current_target.id % 5] if current_target is not None else (0, 0, 0)
         # draw servo line
-        cv2.line(image, servo_box_r, servo_box_l, (255, 255, 255), 2)
+        cv2.line(image, goal_right_point, goal_left_point, (255, 255, 255), 2)
 
         # Use Hough Circle Transform to detect circles with parameters
         circles = cv2.HoughCircles(
@@ -498,7 +446,7 @@ try:
                 # cv2.circle(image, (circle[0], circle[1]), circle[2], (0, 0, 0), 5)
                 try:
                     # check if circle has white center
-                    if gray[circle[1], circle[0]] > 200 and circle[1] < servo_box_l[1]:
+                    if gray[circle[1], circle[0]] > 200 and circle[1] < goal_left_point[1]:
                     # if gray[circle[1], circle[0]] > 200:
                         # append to list if white
                         circle_list.append(circle)
@@ -639,7 +587,7 @@ try:
                     target.catch_x = target.future_points[-1][0]
 
         # choose best target
-        current_target = choose_target(new_targets, trans_matrix)
+        current_target = choose_target(new_targets)
 
         # Uncomment this block if you want to process keyboard input (e.g., press 'q' to exit)
         if key == ord('q'):
@@ -650,51 +598,51 @@ try:
             break
         elif key == ord('p'):
             goal_pos += 50
-            if goal_pos > max_pos:
-                goal_pos = max_pos
+            if goal_pos > MAX_PWM:
+                goal_pos = MAX_PWM
         elif key == ord('l'):
             goal_pos -= 50
-            if goal_pos < min_pos:
-                goal_pos = min_pos
+            if goal_pos < MIN_PWM:
+                goal_pos = MIN_PWM
         elif key == ord('1'):
             circle_params['minDist'] += 1
-            save_values(circle_params)
+            save_params(circle_params)
             print(circle_params)
         elif key == ord('a'):
             circle_params['minDist'] -= 1
-            save_values(circle_params)
+            save_params(circle_params)
             print(circle_params)
         elif key == ord('2'):
             circle_params['param1'] += 1
-            save_values(circle_params)
+            save_params(circle_params)
             print(circle_params)
         elif key == ord('s'):
             circle_params['param1'] -= 1
-            save_values(circle_params)
+            save_params(circle_params)
             print(circle_params)
         elif key == ord('3'):
             circle_params['param2'] += 1
-            save_values(circle_params)
+            save_params(circle_params)
             print(circle_params)
         elif key == ord('d'):
             circle_params['param2'] -= 1
-            save_values(circle_params)
+            save_params(circle_params)
             print(circle_params)
         elif key == ord('4'):
             circle_params['minRadius'] += 1
-            save_values(circle_params)
+            save_params(circle_params)
             print(circle_params)
         elif key == ord('f'):
             circle_params['minRadius'] -= 1
-            save_values(circle_params)
+            save_params(circle_params)
             print(circle_params)
         elif key == ord('5'):
             circle_params['maxRadius'] += 1
-            save_values(circle_params)
+            save_params(circle_params)
             print(circle_params)
         elif key == ord('g'):
             circle_params['maxRadius'] -= 1
-            save_values(circle_params)
+            save_params(circle_params)
             print(circle_params)
         elif key == ord('t'):
             lookup_is_saved = False
